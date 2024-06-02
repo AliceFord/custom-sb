bec9447bce5f159b98095b7bd74a658b06b87234
Ben Walker <146777065+RedstonePilot@users.noreply.github.com>
2024-06-02 15:53:11 +0100

    broken, but kinda works

--- ATCBot/bot.py
+++ ATCBot/bot.py
@@ -1,4 +1,5 @@
-import pygame,random,math
+import pygame,random,math,os
+from prettytable import PrettyTable
 pygame.init()
 font = pygame.font.Font(None,25)
 
@@ -9,26 +10,18 @@ class Bot:
         self.s_width,self.s_height = 800,800
         self.screen = pygame.display.set_mode((self.s_width,self.s_height))
         self.runway_pos = (0,self.s_height//2)
-        pygame.draw.circle(self.screen,(255,255,255),self.runway_pos,5)
-        pygame.draw.line(self.screen, (255,255,255), self.runway_pos,(self.s_width,self.s_height//2),1)
-        for i in range(2, self.s_width, 100):  # Assuming 50 pixels = 1 mile
-            pygame.draw.line(self.screen, (255,255,255), (i, self.s_height // 2 - 10), (i, self.s_height // 2 + 10), 1)
-
-        pygame.draw.line(self.screen, (255,255,255), (550, 0), (550, self.s_height), 1)
-
-        pygame.display.update()
-
-        self.plane_surface = pygame.Surface((self.s_width, self.s_height), pygame.SRCALPHA)
+        
 
     def draw(self):
         self.screen.fill((0,0,0))
         pygame.draw.circle(self.screen,(255,255,255),self.runway_pos,5)
         pygame.draw.line(self.screen, (255,255,255), self.runway_pos,(self.s_width,self.s_height//2),1)
-        for i in range(2, self.s_width, 100):  # Assuming 50 pixels = 1 mile
+        for i in range(2, self.s_width, 100):  #50 pixels = 1 nm
             pygame.draw.line(self.screen, (255,255,255), (i, self.s_height // 2 - 10), (i, self.s_height // 2 + 10), 1)
 
         pygame.draw.line(self.screen, (255,255,255), (550, 0), (550, self.s_height), 1)
-        
+        self.update_landing_order()
+        self.vector()
         for plane in self.planes:
             plane.move()
             pygame.draw.circle(self.screen, (255,0,0), (plane.x, plane.y), 5)
@@ -37,18 +30,42 @@ class Bot:
             self.screen.blit(id_surface, (plane.x, plane.y - 20))
 
         self.display_landing_order()
+        os.system("cls" if os.name == "nt" else "clear")
+        table = PrettyTable(["Plane", "NM to TD","State"])
+        table.clear_rows()
+        for plane in self.landing_order:
+            dist = round(self.get_distance_to_td(plane)/50,2)
+            if plane.ils:
+                state = "ILS"
+            elif plane.base:
+                state = "Base"
+            else:
+                state = int(plane.h)
+            table.add_row([plane.cs,dist,state])
+        print(str(table), end="")
+
+
+
 
         pygame.display.update()
 
 
-    def get_landing_order(self):
-        """gets new planes (as when a handoff occurs) and puts them into a landing order
-        """
-        print("Getting landing order")
-        self.landing_order = sorted(self.planes, key=lambda plane: self.get_distance_to_td(plane))
-        for p in self.landing_order:
-            print(p.cs)
-        self.vector()
+    def update_landing_order(self):
+        for plane in self.planes:
+            if plane not in self.landing_order:
+                new_dist = self.get_distance_to_td(plane)
+                for i in range(len(self.landing_order) - 1):
+                    plane1_distance = self.get_distance_to_td(self.landing_order[i])
+                    plane2_distance = self.get_distance_to_td(self.landing_order[i + 1])
+
+                    if plane2_distance - plane1_distance > 8 * 50:  # 8 nm gap
+                        if plane1_distance < new_dist < plane2_distance:
+                            self.landing_order.insert(i + 1, plane)
+                            return True
+                else: # at the back lol
+                    self.landing_order.append(plane)
+                    return False
+        
 
     def draw_dist(self,plane_a,plane_b):
         pygame.draw.line(self.screen, (0,200,200), (plane_a.x,plane_a.y),(plane_b.x, plane_b.y),1)
@@ -58,8 +75,14 @@ class Bot:
             if i != 0:
                 self.draw_dist(self.landing_order[i-1],p)
 
+        
 
     def get_distance_to_td(self,plane):
+        
+        if plane.base and not plane.ils:
+            return (abs(self.runway_pos[1] - plane.y) + 550)
+        if plane.ils:
+            return plane.x
 
         rad = math.radians(plane.h - 90)
         dx = 550 - plane.x
@@ -72,7 +95,7 @@ class Bot:
 
         distance_on_base = abs(self.runway_pos[1] - turn_y)
         
-        distance_down_ILS = abs(self.runway_pos[0] - 550)
+        distance_down_ILS = 550
         
         return distance_to_base + distance_on_base + distance_down_ILS
     
@@ -130,18 +153,24 @@ class Plane:
         self.d = d
         self.h = 90 if x < 550 else 270
         self.s = 180
+        self.base = False
+        self.ils = False
+        self.landed = False
 
     def move(self):
         if 548 < self.x < 552:
+            self.base = True
             if 398 < self.y < 402:
                 self.h = 270
+                self.ils = True
             else:
                 self.h = 180 if self.y < 400 else 0
+
         if self.x < 2 and 398 < self.y < 402:
-            ... # remove but later
+            self.landed = True
         rad = math.radians(self.h - 90)
-        dx = (self.s/50 / 60 ) * math.cos(rad)
-        dy = (self.s/50 / 60 ) * math.sin(rad)
+        dx = (self.s/50 / 2 ) * math.cos(rad)
+        dy = (self.s/50 / 2 ) * math.sin(rad)
         self.x += dx
         self.y += dy
 
@@ -158,9 +187,6 @@ if __name__ == "__main__":
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx,my = pygame.mouse.get_pos()
                 bot.test_planes(mx,my)
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    bot.get_landing_order()
             
 
     pygame.quit()
